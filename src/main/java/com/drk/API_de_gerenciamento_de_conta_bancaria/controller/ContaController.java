d7de30956474314a61d68ea23f83b752c3e7bc07
DanielKremes <danielkremes06@gmail.com>
2025-07-23 22:52:21 -0300

    feat: finished methods for criarConta, buscarContas, buscarContasPorID

--- src/main/java/com/drk/API_de_gerenciamento_de_conta_bancaria/controller/ContaController.java
+++ src/main/java/com/drk/API_de_gerenciamento_de_conta_bancaria/controller/ContaController.java
@@ -1,9 +1,12 @@
 package com.drk.API_de_gerenciamento_de_conta_bancaria.controller;
 
+import com.drk.API_de_gerenciamento_de_conta_bancaria.entity.Conta;
 import com.drk.API_de_gerenciamento_de_conta_bancaria.service.ContaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/conta")
@@ -11,5 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContaController {
 
     private final ContaService service;
+
+    @PostMapping
+    public ResponseEntity<Conta> criarConta(@RequestBody Conta conta) {
+        return service.criarConta(conta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Conta>> buscarContas() {
+        return service.listarContas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Conta> buscarContasPorID(@PathVariable Long id) {
+        return service.listarContaPorID(id);
+    }
 
 }
--- src/main/java/com/drk/API_de_gerenciamento_de_conta_bancaria/service/ContaService.java
+++ src/main/java/com/drk/API_de_gerenciamento_de_conta_bancaria/service/ContaService.java
@@ -1,11 +1,32 @@
 package com.drk.API_de_gerenciamento_de_conta_bancaria.service;
 
+import com.drk.API_de_gerenciamento_de_conta_bancaria.entity.Conta;
 import com.drk.API_de_gerenciamento_de_conta_bancaria.repository.ContaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ContaService {
     private final ContaRepository repository;
+
+    public ResponseEntity<Conta> criarConta(Conta conta) {
+        return ResponseEntity.ok(repository.save(conta));
+    }
+
+    public ResponseEntity<List<Conta>> listarContas() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    public ResponseEntity<Conta> listarContaPorID(Long id) {
+        Conta existe = repository.findById(id).orElseThrow(
+                ()  -> new EntityNotFoundException("Conta Bancaria não encontrada")
+        );
+        return ResponseEntity.ok(existe);
+    }
+
 }
